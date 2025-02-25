283b4e446c2745989b309848121c328fb4988f64
Sara Guanais de Miranda <guanaissara@gmail.com>
2025-02-24 21:19:24 -0300

    Centralizando itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Signin from "./pages/Signin";
 
 function App() {
-  return <Signin />;
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <Signin />
+  
+    </main>
+  )
 }
 
 export default App;
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,6 +1,9 @@
+import logo from '../assets/logo.png';
+
 export default function Signin() {
     return(
         <div className="flex flex-col items-center justify-around bg-zinc-900 rounded p-8 w-[35rem] h-[35rem]">
+            <img src={logo} className="w-44" />
             <form className="flex flex-col justify-center gap-4 w-full text-2xl text-white">
                 <input 
                     type="email"
@@ -20,6 +23,9 @@ export default function Signin() {
                     Sign in 
                 </button>
             </form>
+        <p className='text-white text-2xl'>
+            Don't have a account? Register
+        </p>
         </div>
     );
 }
